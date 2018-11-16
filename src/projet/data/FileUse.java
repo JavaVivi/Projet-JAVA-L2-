0962cd0471af97aa976e1be191d839b8a83727a7
Virginie C <34030149+JavaVivi@users.noreply.github.com>
2018-11-16 10:16:14 +0100

    Create FileUse.java
    work in progress, searching method for file to add

---
+++ src/projet/data/FileUse.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package projet.data;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * @author vcaze
+ *
+ */
+public class FileUse {
+	private File f;
+
+	/** CONSTRUCTEUR
+	 * @param f
+	 */
+	public FileUse(File f) {
+		f = new File("f");
+	}
+
+	/** GETTERS ET SETTERS
+	 * @return the f
+	 */
+	public File getF() {
+		return f;
+	}
+
+	/**
+	 * @param f the f to set
+	 */
+	public void setF(File f) {
+		this.f = f;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "File [f=" + f + "]";
+	}
+	
+	/** Methode de lecture et d'affichage du fichier, on ne met pas de méthode de modification
+	 * en mode console 
+	 * @param f
+	 */
+	public void file_read(File f) {
+		String line;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			try {
+				while ((line = br.readLine()) != null) {
+					   System.out.println(line);
+					}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				br.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void file_ser(File f) {
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("sauvegarde.ser"));
+			oos.writeObject(f);
+			oos.flush();
+			oos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
