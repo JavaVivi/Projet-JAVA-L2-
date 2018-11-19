45a8e143154f24f54145826bafdc598455219480
Virginie C <34030149+JavaVivi@users.noreply.github.com>
2018-11-19 15:34:54 +0100

    Add files via upload

---
+++ src/projet/data/WrongExtensionException.java
@@ -0,0 +1,25 @@
+package projet.data;
+
+/**
+ * @author vcaze
+ *
+ */
+public class WrongExtensionException extends Exception {
+
+	/**
+	 * Classe d'exception lorsque le type de fichier passé en paramètre
+	 * n'est pas correct
+	 */
+	private static final long serialVersionUID = 3012321050286637944L;
+
+	/**
+	 * @param message
+	 */
+	public WrongExtensionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
