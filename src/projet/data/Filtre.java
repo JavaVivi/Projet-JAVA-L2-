981a14cd48efe5c46b98597e5b657a3224d0ffdd
Virginie C <34030149+JavaVivi@users.noreply.github.com>
2018-11-19 08:23:16 +0100

    Create Filtre.java

---
+++ src/projet/data/Filtre.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package projet.data;
+import java.io.File;
+import java.io.FilenameFilter;
+
+
+/**
+ * @author vcaze
+ *
+ */
+
+//le filtre utilise l'interface FilenameFilter qui permet de filtrer des fichiers ( par 
+//leur extension ici )
+public class Filtre implements FilenameFilter{
+	
+	/* (non-Javadoc)
+	 * @see java.io.FilenameFilter#accept(java.io.File, java.lang.String)
+	 */
+	public boolean accept(File file, String fileName) {
+	return fileName.endsWith(".vcs") || fileName.endsWith(".ics");
+	}
+}
