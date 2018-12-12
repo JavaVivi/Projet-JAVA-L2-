26dedbc108d2977e620caad386089332ff0262ca
Virginie C <34030149+JavaVivi@users.noreply.github.com>
2018-12-12 09:57:48 +0100

    Update Sauvegarde.java

--- src/projet/data/Sauvegarde.java
+++ src/projet/data/Sauvegarde.java
@@ -1,21 +1,33 @@
-/**
- * 
- */
+
 package projet.data;
 
 import java.io.File;
 
 /**
  * @author vcaze
+ * @version 1.0
  *
  */
 public interface Sauvegarde {
+	/**
+	 * @param f
+	 * @throws WrongExtensionException
+	 */
 	public static void lecture_et_sauvegarde_fichier(File f) throws WrongExtensionException {
 	}
+	/**
+	 * @param f
+	 */
 	public static void serialisation(File f) {
 	}
+	/**
+	 * @param f
+	 */
 	public static void serialisation_lecture(File f) {
 	}
+	/**
+	 * @param f
+	 */
 	public static void fragment_HTML(File f) {
 	}
 }
